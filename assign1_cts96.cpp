b50fe6b61d39c56e01b9ca500284678c54bc746a
Cameron Stepanski <100606318+camtstep@users.noreply.github.com>
2022-02-28 10:28:45 -0800

    Create assign1_cts96.cpp

---
+++ assign1_cts96.cpp
@@ -0,0 +1,25 @@
+//File Name: assign1_cts96.cpp
+//
+//Author: Cameron Stepanski
+//Date: 2/3/2020
+//Assignment Number: 1
+//CS 1428.253 Spring 2020
+//Instructor: Jill Seaman
+//
+//This program displays my name, major, and favorite movie
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Name: Cameron Stepanski\n";
+    cout << endl;
+    cout << "Major: Computer Science\n";
+    cout << endl;
+    cout << "Favorite Movie: \"American Psycho\" 2000\n";
+
+    return 0;
+}
